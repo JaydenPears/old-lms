5b750d19882af0b5fe1f099c5e1fb27d9241f99b
JaydenPears <vladimir120905@gmail.com>
2022-11-10 19:58:39 +0300

    Updated the blueprints architecture

--- blueprints/course/course.py
+++ blueprints/course/course.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template
 
-course = Blueprint('course', __name__, template_folder='templates', static_folder='static')
+course = Blueprint('course', __name__,
+                   template_folder='../templates',
+                   static_folder='../static',
+                   static_url_path='course-static')
 
 
 @course.route("/")
@@ -9,4 +12,4 @@ def index():
         'title': "Главная страница",
         'text': 123,
     }
-    return render_template("course.html", **context)
+    return render_template("course/course.html", **context)
